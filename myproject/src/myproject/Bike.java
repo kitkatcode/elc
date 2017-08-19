5d4835b468224e2dda2ae9eaeb3c905fab26f153
kitkatcode <lafar2@gmail.com>
2017-08-19 12:49:45 +0200

    message

---
+++ myproject/src/myproject/Bike.java
@@ -0,0 +1,12 @@
+package myproject;
+
+class Bike {
+int numerOfWheel = 90;
+	void run() {System.out.println("running");}
+}
+ class Splender extends Bike {
+	 int numerOfWheel = 150;	 
+	 void run() {System.out.println("running safely with 60km");}
+
+}
+
